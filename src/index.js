fb9580258a8e78db8ac9b3271b4b87f714d33f5e
zwt <1255284280@qq.com>
2020-06-03 10:03:12 +0800

    管理资源

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import _ from 'lodash'
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
-  
+
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    // Lodash, now imported by this script
+    // Lodash, 现在通过 script 标签导入
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    element.classList.add('hello');
+
+    // 将图像添加到我们现有的 div。
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
+
     return element;
-  }
-  
-  document.body.appendChild(component());
+}
+
+document.body.appendChild(component());
